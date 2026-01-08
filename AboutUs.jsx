f0ac1d50335aaaaf5e30ab2b8f248821fdbf63ef
mpotsengheisi <165156065+mpotsengheisi@users.noreply.github.com>
2026-01-08 09:25:42 +0200

    Create AboutUs component for company information

---
+++ AboutUs.jsx
@@ -0,0 +1,14 @@
+// AboutUs.jsx
+import React from 'react';
+
+const AboutUs = () => {
+  return (
+    <div>
+      <h1>About Our Company</h1>
+      <p>Company details here...</p>
+      {/* Add mission, team, contact info, etc. */}
+    </div>
+  );
+};
+
+export default AboutUs;
